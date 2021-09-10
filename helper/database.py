c384a15e58b27a0f70ac3d4711bc1446b087fb23
ABHINAV PATHAK <87684426+GhostXBeast@users.noreply.github.com>
2021-09-10 09:38:16 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -1,8 +1,8 @@
 import pymongo 
 import os
 
-DB_NAME = os.environ.get("DB_NAME","")
-DB_URL = os.environ.get("DB_URL","")
+DB_NAME = os.environ.get("DB_NAME","Cluster0")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://Anon956:GlACEljYJAEEOI9k@cluster0.bng7u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 mongo = pymongo.MongoClient(DB_URL)
 db = mongo[DB_NAME]
 dbcol = db["USER"]
